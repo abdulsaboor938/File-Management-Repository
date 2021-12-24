030c6f28e201315bb87402cd21866ec4645b7968
Abdul Saboor <66201863+abdulsaboor938@users.noreply.github.com>
2021-12-24 18:47:26 +0500

    added userClass in q1.h
    Added userClass implementation in q1.h file.

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include"q1.h"
 using namespace std;
 
 int main()
 {
-	cout << "hello World" << endl;
+	userClass u1(1234, false), u2(3456, true);
+	cout << u1 << u2 << u1 << endl;
 	system("pause");
 	return 0;
 }
---
+++ q1.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// definition of user class
+class userClass
+{
+	int id;
+	bool operation; // false is read, true is write
+
+public:
+	// constructor of class
+	userClass(int temp_id, bool temp_op) :id(temp_id), operation(temp_op) {}
+	friend ostream& operator<<(ostream& out, userClass& temp)
+	{
+		if (temp.operation == false)
+			out << "User ID : " << temp.id << ", wants to read from this file" << endl;
+		else
+			out << "User ID : " << temp.id << ", wants to write on this file" << endl;
+		return out;
+	}
+};
